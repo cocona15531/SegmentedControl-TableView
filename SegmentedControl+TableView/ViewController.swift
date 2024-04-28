b5e8f770c3f904959d83e7c4c94c58b8af6f17d7
issei.ueda <ron.ron633662@gmail.com>
2024-04-28 12:56:48 +0900

    TableViewを表示させた

--- SegmentedControl+TableView/ViewController.swift
+++ SegmentedControl+TableView/ViewController.swift
@@ -15,10 +15,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "demo"
+        tableView.dataSource = self
     }
 
     @IBAction func selectedSegment(_ sender: Any) {
     }
     
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 8
+    }
 }
 
