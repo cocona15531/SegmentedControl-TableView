e9565b0928a5bfabe895c043cac4be34e9cd8928
issei.ueda <ron.ron633662@gmail.com>
2024-04-28 12:47:38 +0900

    コンポーネントをViewControllerに紐付け

--- SegmentedControl+TableView/ViewController.swift
+++ SegmentedControl+TableView/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "demo"
     }
 
-
+    @IBAction func selectedSegment(_ sender: Any) {
+    }
+    
 }
 
