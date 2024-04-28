5ab4b7e7560fe424aa3eab6b359343512f18a201
issei.ueda <ron.ron633662@gmail.com>
2024-04-28 12:39:57 +0900

    first commit

---
+++ SegmentedControl+TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SegmentedControl+TableView
+//
+//  Created by Issei Ueda on 2024/04/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "demo"
+    }
+
+
+}
+
